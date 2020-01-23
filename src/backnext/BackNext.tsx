d74be72047a11f0f3806325f39c45e057e2b7b6b
fishyFrogFace <camillahallo@mail.com>
2020-01-23 22:23:12 +0100

    Conditional rendering of components

---
+++ src/backnext/BackNext.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const BackNext: React.FC = () => {
+  return (
+    <div className='App'>
+        <button className='Index-button'>
+          Next
+        </button>
+    </div>
+  );
+}
+
+export default BackNext;
