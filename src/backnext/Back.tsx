c64fa340faba4987b11681a61e8d82d908aea8d7
Camilla Rygh <camilla.rygh@asolvi.com>
2020-01-28 18:10:50 +0100

    Empty NavBar, kitten image on Back.tsx

--- src/backnext/Back.tsx
+++ src/backnext/Back.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import '../App.css';
-import { Page } from '../App'
+import 'App.css';
+import { Page } from 'App'
+import NavBar from 'components/NavBar'
 
 interface Props {
     changePage: (page: Page) => void
@@ -8,14 +9,20 @@ interface Props {
 
 const Back: React.FC<Props> = props => {
     return (
+        <div className='outer'>
+        <NavBar />
+        <div className='imageContainer'>
+          <img src={"kitten.svg"} alt="Penguin"/>
+        </div>
         <div className='backnext'>
             <button className='btn' onClick={() => props.changePage(Page.Next)}>
-                Back
+              Back
             </button>
             <button className='btn' onClick={() => props.changePage(Page.Back)}>
-                Next
+              Next
             </button>
         </div>
+      </div>
     );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+const NavBar: React.FC<{}> = props => {
+  return (
+    <div className='navBar'>
+    </div>
+  );
+}
+
+export default NavBar;
