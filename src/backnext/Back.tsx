3c72510630757ec1e04a75f27d43660f056abadf
fishyFrogFace <camillahallo@mail.com>
2020-02-01 17:08:46 +0100

    Formatting

--- src/backnext/Back.tsx
+++ src/backnext/Back.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import '../App.css';
-import { Page } from '../App'
-import NavBar from '../components/NavBar'
-import Button from '../components/Button'
-import kitten from './kitten.svg';
+import React from "react";
+import "../App.css";
+import { Page } from "../App";
+import NavBar from "../components/NavBar";
+import Button from "../components/Button";
+import kitten from "./kitten.svg";
 
 interface Props {
-    changePage: (page: Page) => void
+  changePage: (page: Page) => void;
 }
 
 const Back: React.FC<Props> = props => {
-    return (
-        <div className='outer'>
-        <NavBar />
-        <div className='imageContainer'>
-          <img src={kitten} alt="Kitten"/>
-        </div>
-        <div className='backnext'>
-            <Button onClick={() => props.changePage(Page.Next)}>
-              &lt;Back
-            </Button>
-            <Button onClick={() => props.changePage(Page.Back)}>
-              Next&gt;
-            </Button>
-        </div>
+  return (
+    <div className="outer">
+      <NavBar />
+      <div className="imageContainer">
+        <img src={kitten} alt="Kitten" />
       </div>
-    );
-}
+      <div className="backnext">
+        <Button onClick={() => props.changePage(Page.Next)}>
+          &lt;Back
+        </Button>
+        <Button onClick={() => props.changePage(Page.Back)}>
+          Next&gt;
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Back;
--- src/backnext/Next.tsx
+++ src/backnext/Next.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import '../App.css';
-import { Page } from '../App'
-import NavBar from '../components/NavBar'
-import Button from '../components/Button'
-import penguin from './penguin.svg';
+import React from "react";
+import "../App.css";
+import { Page } from "../App";
+import NavBar from "../components/NavBar";
+import Button from "../components/Button";
+import penguin from "./penguin.svg";
 
 interface Props {
-    changePage: (page: Page) => void
+  changePage: (page: Page) => void;
 }
 
 const Next: React.FC<Props> = props => {
   return (
-    <div className='outer'>
+    <div className="outer">
       <NavBar />
-      <div className='imageContainer'>
-        <img src={penguin} alt="Penguin"/>
+      <div className="imageContainer">
+        <img src={penguin} alt="Penguin" />
       </div>
-      <div className='backnext'>
-           <Button onClick={() => props.changePage(Page.Next)}>
-              &lt;Back
-           </Button>
-            <Button onClick={() => props.changePage(Page.Back)}>
-              Next&gt;
-            </Button>
+      <div className="backnext">
+        <Button onClick={() => props.changePage(Page.Next)}>
+          &lt;Back
+        </Button>
+        <Button onClick={() => props.changePage(Page.Back)}>
+          Next&gt;
+        </Button>
       </div>
     </div>
   );
-}
+};
 
 export default Next;
