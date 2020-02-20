f090e08b78ed42812d90a151b497f39bf6f950ec
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-20 18:13:18 +0100

    Write tests for new path through application

--- cypress/integration/end-to-end.spec.js
+++ cypress/integration/end-to-end.spec.js
@@ -1,45 +1,57 @@
 /// <reference types="Cypress" />
 
 context('End-to-end', () => {
-    before(() => {
-      cy.visit('http://localhost:3000/')
-    })
+  before(() => {
+    cy.visit('http://localhost:3000/')
+  })
 
-    it('front page contains an examination button', () => {
-        cy.get('.btn')
-          .first()
-          .should('contain', 'Gerd - Lvl 1')
-    })
+  it('front page contains an examination button', () => {
+    cy.get('.btn')
+      .first()
+      .should('contain', 'Gerd - Lvl 1')
+  })
 
-    it('start page contains a navigation menu', () => {
-      cy.get('.navBar')
+  it('front page contains a navigation menu', () => {
+    cy.get('.navBar')
       .first()
       .should('be.visible')
-    })
+  })
 
-    it('start button is clickable and renders 2nd component', () => {
+  it('front page button is clickable and renders first question', () => {
+    cy.get('.btn').first().click()
+
+    cy.get('.btn')
+      .first()
+      .should('contain', 'Start')
+  })
+
+  it('first question contains a navigation menu', () => {
+    cy.get('.navBar')
+      .first()
+      .should('be.visible')
+  })
+
+  it('first question button is clickable and renders second question', () => {
     cy.get('.btn').first().click()
 
     cy.get('.h1')
       .first()
       .should('be.contain', 'Question component here')
-    })
+  })
 
-    it('2nd page contains a navigation menu', () => {
-      cy.get('.navBar')
+  it('second question contains a navigation menu', () => {
+    cy.get('.navBar')
       .first()
       .should('be.visible')
-    })
-
-    it('a reload brings you to start page', () => {
-        cy.reload()
-        cy.get('.btn')
-          .first()
-          .should('contain', 'Gerd - Lvl 1')
-    
-        // reload the page without using the cache
-        //cy.reload(true)
-    })
-
   })
-  
+
+  it('a reload brings you to start page', () => {
+    cy.reload()
+    cy.get('.btn')
+      .first()
+      .should('contain', 'Gerd - Lvl 1')
+
+    // reload the page without using the cache
+    //cy.reload(true)
+  })
+})
