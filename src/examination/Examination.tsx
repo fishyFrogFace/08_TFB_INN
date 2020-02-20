31e1571d795ba44c5a08427ca8c8273a692b94ba
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-20 18:01:53 +0100

    Add functions for looping through questions inside examination

--- src/examination/Examination.tsx
+++ src/examination/Examination.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 import '../App.css';
+import { Int } from '../Helpers'
 import NavBar from '../components/NavBar'
+import Start from '../questions/Start'
 
-const FrontPage: React.FC<{}> = props => {
+interface State {
+  currentQuestion: Int,
+  pages: [any]
+
+}
+
+const Examination: React.FC<{}> = props => {
+  const getResult = () => {
+    setCurrentQuestion(currentQuestion + 1)
+  };
+
+  const [pages, setPages] = useState([<Start getResult={getResult} />, <h1 className='h1'>Question component here</h1>]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
   return (
     <div className="outer">
       <NavBar />
       <div className='main'>
-         <h1 className='h1'>Question component here</h1>
+        {pages[currentQuestion]}
       </div>
     </div>
   );
 }
 
-export default FrontPage;
+export default Examination;
