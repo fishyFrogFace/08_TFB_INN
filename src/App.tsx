33f2702ab014df297c8ff356e5d87b6a0377bd0a
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-22 14:30:16 +0100

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,28 @@ const App: React.FC<{}> = props => {
   const [currentPage, setCurrentPage] = useState(Page.FrontPage);
 
   const changePage = (page: Page) => {
-      setCurrentPage(page)
+    setCurrentPage(page)
   };
 
   const example = [
-                    { name: "Lvl 1", username: "", avatar: "jadda", status: 'new' },
-                    { name: "Lvl 2", username: "", avatar: "avatar", status: 'new' },
-                    { name: "Lvl 2", username: "Gerd", avatar: "jadda", status: 'paused' },
-                    { name: "Lvl 2", username: "Peder", avatar: "avatar", status: 'paused' },
-                    { name: "Lvl 2", username: "Ali", avatar: "jadda", status: 'paused' },
-                   { name: "Lvl 2", username: "Ole", avatar: "avatar", status: 'paused' }
-                  ]
+    { name: "Lvl 1", username: "", avatar: "jadda", status: 'new' },
+    { name: "Lvl 2", username: "", avatar: "avatar", status: 'new' },
+    { name: "Lvl 2", username: "Gerd", avatar: "jadda", status: 'paused' },
+    { name: "Lvl 2", username: "Peder", avatar: "avatar", status: 'paused' },
+    { name: "Lvl 2", username: "Ali", avatar: "jadda", status: 'paused' },
+    { name: "Lvl 2", username: "Ole", avatar: "avatar", status: 'paused' }
+  ]
 
   switch (currentPage) {
-      case Page.FrontPage:
-          /* fetch available examinations from local storage (or backend API) and pass
-             them to FrontPage */
-          return <FrontPage availableExaminations={example} changePage={changePage} />
+    case Page.FrontPage:
+      /* fetch available examinations from local storage (or backend API) and pass
+         them to FrontPage */
+      return <FrontPage availableExaminations={example} changePage={changePage} />
 
-          /* fetch questions and question props from local storage (or backend API)
-             and pass them to Examination */
-      case Page.Examination:
-          return <Examination />
+    /* fetch questions and question props from local storage (or backend API)
+       and pass them to Examination */
+    case Page.Examination:
+      return <Examination />
   }
 }
 
