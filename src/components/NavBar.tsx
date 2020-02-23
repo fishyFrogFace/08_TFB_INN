1ae0a4710404a5d28206c84d8da218686f36fd7c
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-23 18:30:35 +0100

    Change all visible text to Norwegian

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar: React.FC<{}> = props => {
   return (
     <div className='navBar'>
-      <h1>This is a navigation bar</h1>
+      <h1>Dette er en navigasjonsmeny</h1>
     </div>
   );
 }
