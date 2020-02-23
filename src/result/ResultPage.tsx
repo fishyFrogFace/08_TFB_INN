3b86834969cbfe52e80890eca59336194cd5c04b
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-23 19:20:17 +0100

    Examination sends username to ResultPage

--- src/result/ResultPage.tsx
+++ src/result/ResultPage.tsx
@@ -3,17 +3,12 @@ import './ResultPage.css';
 import ProgressBar from 'components/ProgressBar';
 import { Result } from '../examination/Examination';
 
-interface Props {
-  results: Result[];
-}
-
-const ResultPage: React.FC<Props> = props => {
-  const [results] = useState(props.results);
+const ResultPage: React.FC<Result> = props => {
 
   return (
     <div className='resultContainer'>
-      <h1 className='h1'>Resultat</h1>
-      {results.map(element => {
+      <h1 className='h1'>Resultat for {props.username}</h1>
+      {props.results.map(element => {
         return <ProgressBar {...element} />;
       })}
     </div>
