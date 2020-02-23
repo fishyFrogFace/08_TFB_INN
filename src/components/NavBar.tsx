01728b0b4283905bb10656fb70228f2265e2bb80
fishyFrogFace <camillmg@stud.ntnu.no>
2020-02-23 10:54:40 +0100

    Add header to NavBar, to make it clear what it is

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import '../App.css';
+import './NavBar.css';
 
 const NavBar: React.FC<{}> = props => {
   return (
     <div className='navBar'>
+      <h1>This is a navigation bar</h1>
     </div>
   );
 }
