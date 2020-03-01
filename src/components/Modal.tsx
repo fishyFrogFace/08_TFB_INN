b1a3235655dd0eecc84121f8dbf6ea1bfd55a3a8
fishyFrogFace <camillmg@stud.ntnu.no>
2020-03-01 14:32:09 +0100

    Create a modal and break all the things!

---
+++ src/components/Modal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Modal.css';
+
+interface Props {
+  show: boolean;
+  onClick: () => void;
+}
+
+const Modal: React.FC<Props> = props => {
+  {
+    if (props.show) {
+      return (
+        <div className='modal fade' id='modal' role='dialog'>
+          <div className='modal-header'>
+            <button
+              type='button'
+              className='close'
+              onClick={() => props.onClick()}>
+              &times;
+            </button>
+            <h4 className='modal-title'>Modal Header</h4>
+          </div>
+          <div className='modal-body'>
+            <p>This is a small modal.</p>
+          </div>
+          <div className='modal-footer'>
+            <button
+              type='button'
+              className='modal-btn'
+              onClick={() => props.onClick()}>
+              Close
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+};
+
+export default Modal;
