cfeb32c63bb753dd70b03646de438dd1699ea585
fishyFrogFace <camillmg@stud.ntnu.no>
2020-03-09 22:41:36 +0100

    What if Cypress is enough?

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders start page', () => {
-  const { getByText } = render(<App />);
-  const buttonElements = getByText(/Tittel/i);
-  expect(buttonElements).toBeInTheDocument();
-});
