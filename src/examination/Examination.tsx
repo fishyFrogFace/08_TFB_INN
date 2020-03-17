bda239d9d515e40ceff1aa813e907bad2fcd7544
Camilla Rygh <camillahallo@mail.com>
2020-03-17 22:28:57 +0100

    Result gets rendered by Examination

--- src/examination/Examination.tsx
+++ src/examination/Examination.tsx
@@ -4,11 +4,11 @@ import NavBar from '../components/NavBar';
 import ResultPage from '../result/ResultPage';
 import EnterName from '../exampages/EnterName';
 import {
-  Result,
   Page,
   ExamState,
   ExamDefinition,
-  ExamPage
+  ExamPage,
+  QuestionResult
 } from '../Types';
 import Subject from './Subject';
 
@@ -26,46 +26,53 @@ const Examination: React.FC<Props> = props => {
   const [currentSubject, setCurrentSubject] = useState(
     props.examState.currentSubject
   );
-  const [questions] = useState(
-    props.examDefinition.subjects[currentSubject].questions
+  const [result, setResult] = useState(props.examState.results);
+  const [examPage, setExamPage] = useState(
+    props.examState.instanceID === 0 ? ExamPage.EnterName : ExamPage.Subject
   );
-  const [result, setResult] = useState({
-    username: props.examState.username,
-    results: props.examState.results
-  });
-  const [examPage, setExamPage] = useState(props.examState.instanceID === 0 ? ExamPage.EnterName : ExamPage.Subject);
+  const [username, setUsername] = useState(props.examState.username);
 
   const getUsername = (username: string) => {
-    setResult((res: Result) => ({ ...res, username: username }));
+    setUsername(username);
     // tell the ouside world e.g. App about this change in state
     setExamPage(ExamPage.Subject);
+  };
+
+  const getSubjectInfo = (
+    currentQuestion: number,
+    result: QuestionResult[]
+  ) => {
+    setCurrentQuestion(currentQuestion);
+    setResult(result);
   };
 
   const choosePage = (page: ExamPage) => {
     switch (page) {
       case ExamPage.Subject:
-          console.log("Subject: " + props.examState.instanceID)
+        console.log('Subject: ' + props.examState.instanceID);
         return (
-            <Subject
+          <Subject
             examState={props.examState}
             examDefinition={props.examDefinition}
             changePage={props.changePage}
             storeExam={props.storeExam}
+            alertExamination={getSubjectInfo}
+            examOver={examOver}
           />
         );
 
       case ExamPage.EnterName:
-          console.log("EnterName: " + props.examState.instanceID)
+        console.log('EnterName: ' + props.examState.instanceID);
         return (
           <EnterName
-            avatar={"thing"} //TODO send real avatar here when we have that story ready
+            avatar={'thing'} //TODO send real avatar here when we have that story ready
             getUsername={getUsername}
           />
         );
 
-      case ExamPage.Exit:
+      case ExamPage.Results:
         // TODO let App know the examination is over
-        return <ResultPage {...result} />;
+        return <ResultPage username={username} result={result} />;
     }
   };
 
@@ -80,17 +87,20 @@ const Examination: React.FC<Props> = props => {
       instanceID: props.examState.instanceID,
       currentQuestion: currentQuestion,
       currentSubject: currentSubject,
-      results: result.results,
-      username: result.username
+      results: result,
+      username: username
     };
     props.storeExam(data);
   };
 
+  const examOver = (result: QuestionResult[]) => {
+    setResult(result);
+    setExamPage(ExamPage.Results);
+  };
+
   return (
     <div className='main'>
-        <p>Waaaat</p>
       <NavBar quitExam={quitExam} pauseExam={pauseExam} />
-      {console.log("why not here")}
       {choosePage(examPage)}
     </div>
   );
