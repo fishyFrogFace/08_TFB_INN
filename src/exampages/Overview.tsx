0d114fe5bf02479ddb382e26734de603ac881620
Camilla Rygh <camillahallo@mail.com>
2020-03-24 00:43:16 +0100

    Styling of overview

--- src/exampages/Overview.tsx
+++ src/exampages/Overview.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css';
 import './Pages.css';
 import Button from '../components/Button';
@@ -6,16 +6,24 @@ import CircularProgressBar from '../components/CircularProgressBar';
 import avatar from './big-pink.png';
 
 interface Props {
-  startExam: () => void
+  subjectTitles: string[];
+  startExam: () => void;
 }
 
 const Overview: React.FC<Props> = props => {
   return (
     <div className='questionContainer'>
-      <CircularProgressBar />
-      <Button classNames='next' onClick={() => props.startExam()}>
-        Neste
-      </Button>
+      <h1 className='h1 overview-header'>Mine temaer</h1>
+      <div className='subjectContainer'>
+        {props.subjectTitles.map(subjectTitle => {
+          return (
+            <Button classNames='subject-btn' onClick={props.startExam}>
+              <h2 className='subjectTitle'>{subjectTitle}</h2>
+              <CircularProgressBar />
+            </Button>
+          );
+        })}
+      </div>
     </div>
   );
 };
