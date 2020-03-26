bd498aa9973d9fd7b53510151918f79b7349c438
Camilla Rygh <camillahallo@mail.com>
2020-03-26 19:17:43 +0100

    Calculate completion

--- src/components/CircularProgressBar.tsx
+++ src/components/CircularProgressBar.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './CircularProgressBar.css';
-import { QuestionResult } from '../Types';
 
-const ProgressBar: React.FC<{}> = props => {
-  // const calculatePercentage = () => {
-  //   return (props.pointsAchieved / props.maxPoints) * 100;
-  // };
+interface Props {
+  completed: number;
+  total: number;
+}
+
+const ProgressBar: React.FC<Props> = props => {
+  const calculatePercentage = () => {
+    return Math.round((props.completed / props.total) * 100);
+  };
 
   return (
-    <div className='progress-circle p10'>
-      <span>10%</span>
+    <div className={`progress-circle p${calculatePercentage()}`}>
+      <span>{calculatePercentage()}%</span>
       <div className='left-half-clipper'>
         <div className='first50-bar'></div>
         <div className='value-bar'></div>
