91cebdff961e69c520f3b992826f0cfbab219a02
fishyFrogFace <camillahallo@mail.com>
2020-04-21 21:12:39 +0200

    Coordinates in WhereInPicture are functional

--- src/questions/WhereInPicture.tsx
+++ src/questions/WhereInPicture.tsx
@@ -16,15 +16,25 @@ const WhereInPicture: React.FC<Props> = props => {
   // set a randomly generated name that will be kept if user doesn't type anything
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [points, setPoints] = useState(props.maxPoints);
-  const [clickedWhileCorrect, setClickedWhileCorrect] = useState(false)
+  const [clickedWhileCorrect, setClickedWhileCorrect] = useState(false);
+  //for debugging
+  const [foo, setFoo] = useState('false');
+  const [bar, setBar] = useState('false');
+  //for debugging
 
-  const checkInput = () => {
-    const xInArea = position.x >= props.imageInformation.min.x && position.x <= props.imageInformation.max.x
-    const yInArea = position.y >= props.imageInformation.min.y && position.y <= props.imageInformation.max.y
-    console.log(xInArea, yInArea)
+  const checkInput = (x, y) => {
+    const xInArea =
+      x >= props.imageInformation.min.x && x <= props.imageInformation.max.x;
+    const yInArea =
+      y >= props.imageInformation.min.y && y <= props.imageInformation.max.y;
+    //for debugging
+    const blah = p => (p ? 'true' : 'false');
+    setFoo(blah(xInArea));
+    setBar(blah(yInArea));
+    //for debugging
     if (xInArea && yInArea) {
       // feedback to user
-      setClickedWhileCorrect(true)
+      setClickedWhileCorrect(true);
     } else {
       const newPoints = points > 0 ? points - 1 : 0;
       setPoints(newPoints);
@@ -33,16 +43,16 @@ const WhereInPicture: React.FC<Props> = props => {
   };
 
   const onNext = () => {
-          // disabled if not correct
-          props.updateResult({
-            maxPoints: props.maxPoints,
-            resultTitle: props.resultTitle,
-            pointsAchieved: points
-          });
-  }
+    // disabled if not correct
+    props.updateResult({
+      maxPoints: props.maxPoints,
+      resultTitle: props.resultTitle,
+      pointsAchieved: points
+    });
+  };
 
-// onClick might not work on touch screens, but is harder to test in development
-// due to the application not being live. will test in production branch
+  // onClick might not work on touch screens, but is harder to test in development
+  // due to the application not being live. will test in production branch
   return (
     <div>
       <h1 className='h1'>{props.text}</h1>
@@ -52,13 +62,31 @@ const WhereInPicture: React.FC<Props> = props => {
         <img
           className='whereInPictureImg'
           onClick={e => {
-            setPosition({ x: e.clientX, y: e.clientY })
-            checkInput()
+            const xPos = e.pageX - e.currentTarget.offsetLeft;
+            const yPos = e.pageY - e.currentTarget.offsetTop;
+            //for debugging
+            setPosition({
+              x: e.pageX - e.currentTarget.offsetLeft,
+              y: e.pageY - e.currentTarget.offsetTop
+            });
+            //for debugging
+            checkInput(xPos, yPos);
           }}
           src={props.imageInformation.image}
+          alt={props.text}
         />
-        <p>X: {position.x}, Y: {position.y}</p>
-        <Button classNames='next' onClick={() => onNext()} disabled={!clickedWhileCorrect}>
+
+        <p>
+          X: {position.x}, Y: {position.y}
+        </p>
+        <p>
+          {foo}, {bar}
+        </p>
+
+        <Button
+          classNames='next'
+          onClick={() => onNext()}
+          disabled={!clickedWhileCorrect}>
           Neste
         </Button>
       </form>
