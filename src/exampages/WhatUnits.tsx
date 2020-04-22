74ee01c8e2005c2ac7a6872fc790863a8730f6ff
Camilla Rygh <camillahallo@mail.com>
2020-04-22 18:38:41 +0200

    Styling of WhatUnits

--- src/exampages/WhatUnits.tsx
+++ src/exampages/WhatUnits.tsx
@@ -27,18 +27,17 @@ const WhatUnits: React.FC<PropsFromRedux> = props => {
   return (
     <div className='questionContainer'>
       <h1 className='h1'>Hvilke enheter har du?</h1>
-      <div className='buttoncontainer'>
+      <div className='button-container'>
         {units.map((item, i) => (
-          <div className='imageWithDescription'>
+          <div className='image-with-description' key={i}>
             <Button
-              key={i}
-              classNames={selectedButtons.includes(i) ? 'selected' : ''}
+              classNames={`unit-btn ${
+                selectedButtons.includes(i) ? 'selected' : ''
+              }`}
               onClick={() => updateAnswer(i)}>
-              <img src={item.image} alt='' />
+              <img className='unit-img' src={item.image} alt='' />
             </Button>
-            <h2 className='h2' key={i}>
-              {item.description}
-            </h2>
+            <h2 className='h2'>{item.description}</h2>
           </div>
         ))}
       </div>
