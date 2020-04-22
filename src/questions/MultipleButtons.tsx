b5d86fd8700830f3c9634cf2fcbe1b5905aa6ca6
fishyFrogFace <camillahallo@mail.com>
2020-04-22 13:32:13 +0200

    Formatting

--- src/questions/MultipleButtons.tsx
+++ src/questions/MultipleButtons.tsx
@@ -4,58 +4,54 @@ import './Question.css';
 import Button from '../components/Button';
 import { QuestionResult } from '../Types';
 
-
 interface Props {
   text: string; //hva det blir spurt om
   maxPoints: number; //1
   resultTitle: string; //kartleggingsmål
-  isImage: boolean; 
+  isImage: boolean;
   answerValues: string[]; //eksternt ?
   correctAlt: string; //riktig alternativ
   updateResult: (result: QuestionResult) => void; //??
 }
 
-
 const MultipleButtons: React.FC<Props> = props => {
   const [selectedButtons, setSelectedButtons] = useState<number[]>([]);
-
 
   const updateAnswer = (value: number) => {
     if (selectedButtons.find(element => element === value) === undefined) {
       setSelectedButtons(selectedButtons.concat([value]));
-    }
-    else {
+    } else {
       setSelectedButtons(selectedButtons.filter(element => element !== value));
     }
-    console.log(value); 
-  }
-  
+    console.log(value);
+  };
+
   const returnResult = () => {
     props.updateResult({
       maxPoints: props.maxPoints,
       resultTitle: props.resultTitle,
       pointsAchieved: 0 //TODO Fikses senere
-    })
-  }
- 
+    });
+  };
+
   return (
     <div>
       <h1 className='h1'>{props.text}</h1>
-      <div className="buttoncontainer">
-        
-          {props.answerValues.map((item, i) =>
-              <Button key={i} classNames={selectedButtons.includes(i) ? "selected" : ""} onClick={() => updateAnswer(i)}>
-                {props.isImage ? <img src={item} alt=""/> : item}
-              </Button>
-          )}
-        
+      <div className='buttoncontainer'>
+        {props.answerValues.map((item, i) => (
+          <Button
+            key={i}
+            classNames={selectedButtons.includes(i) ? 'selected' : ''}
+            onClick={() => updateAnswer(i)}>
+            {props.isImage ? <img src={item} alt='' /> : item}
+          </Button>
+        ))}
       </div>
       <div>
         <Button classNames='next' onClick={returnResult}>
           Neste
         </Button>
       </div>
-
     </div>
   );
 };
