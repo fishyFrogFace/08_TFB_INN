80bd72051bafc2c40da01bd7024a1bfce61211ac
fishyFrogFace <camillahallo@mail.com>
2020-04-23 23:23:24 +0200

    Restructure tests and write new for new component

---
+++ cypress/integration/passwords-and-login.spec.js
@@ -0,0 +1,117 @@
+/// <reference types="Cypress" />
+
+context('Passwords-and-login', () => {
+  const subjectTitle = 'Passord og innlogging';
+
+  before(() => {
+    cy.visit('http://localhost:3000/');
+
+    // start examination
+    cy.get('.examination-startbutton')
+      .first()
+      .click();
+
+    // skip writing name
+    cy.get('.next')
+      .first()
+      .click();
+
+    // skip choosing units
+    cy.get('.next')
+      .first()
+      .click();
+  });
+
+  it('overview contains the subject', () => {
+    cy.get('.subject-btn')
+      .eq(1)
+      .should('contain', subjectTitle);
+  });
+
+  it('subject button is clickable and renders create password', () => {
+    cy.get('.subject-btn')
+      .eq(1)
+      .click();
+
+    cy.get('.h1')
+      .first()
+      .should('contain', 'Lag et sikkert passord');
+  });
+
+  it('create password contains a navigation menu', () => {
+    cy.get('.navBar')
+      .first()
+      .should('be.visible');
+  });
+
+  it('password button is clickable and renders copytext 2', () => {
+    cy.get('.inputField')
+      .first()
+      .type('KokosBollerEr-Godt!1');
+
+    cy.get('.next')
+      .first()
+      .click();
+
+    cy.get('.h1')
+      .first()
+      .should('contain', 'This is totally another subject');
+  });
+
+  it('copytext 2 contains a navigation menu', () => {
+    cy.get('.navBar')
+      .first()
+      .should('be.visible');
+  });
+
+  it('copytext 2 button is clickable and renders success screen 2', () => {
+    cy.get('.inputField')
+      .first()
+      .type('This is totally another subject');
+
+    cy.get('.next')
+      .first()
+      .click();
+
+    cy.get('.next')
+      .first()
+      .click();
+
+    cy.get('.h1')
+      .first()
+      .should('contain', 'Du har fullført Passord og innlogging!');
+  });
+
+  it('success screen 2 contains a navigation menu', () => {
+    cy.get('.navBar')
+      .first()
+      .should('be.visible');
+  });
+
+  it('success screen 2 is clickable and renders overview', () => {
+    cy.get('.next')
+      .first()
+      .click();
+
+    cy.get('.subject-btn')
+      .eq(1)
+      .should('be.disabled');
+  });
+
+  it('overview button is clickable and renders result', () => {
+    cy.get('.next')
+      .first()
+      .click();
+
+    cy.get('.h1')
+      .first()
+      .should('contain', 'Resultat');
+  });
+
+  it('result reflects what the user achieved', () => {
+    const width = ['400px', '400px'];
+    cy.get('.filler').each(($el, i) => {
+      cy.wrap($el).should('have.css', 'width', width[i]);
+    });
+  });
+});
